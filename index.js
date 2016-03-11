946135ea214a25ca16c241d4b5783153ba8fe8a7
Christophe Hurpeau <christophe@hurpeau.com>
2016-03-11 01:47:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function deepFreeze(obj) {
+    if (obj instanceof Map) {
+        obj.clear = obj.delete = obj.set = function () {
+            throw new Error('map is read-only');
+        };
+    } else if (obj instanceof Set) {
+        obj.add = obj.clear = obj.delete = function () {
+            throw new Error('set is read-only');
+        };
+    }
+
+    Object.getOwnPropertyNames(obj).forEach(function (name) {
+        var prop = obj[name];
+
+        // Freeze prop if it is an object
+        if (typeof prop == 'object' && !Object.isFrozen(prop)) {
+            deepFreeze(prop);
+        }
+    });
+
+    // Freeze self
+    return Object.freeze(obj);
+}
